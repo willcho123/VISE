4c9a4eb78fa0a579fbaad887ca00a08d8919e34e
wilches <willchezandressalas@gmail.com>
2025-10-27 10:53:32 -0500

    Cambios locales antes del pull

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+ import appInsights from "applicationinsights";
+
 // server.js
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -14,3 +16,20 @@ const purchaseRoutes = require("./src/routes/purchase.routes");
 app.use("/purchase", purchaseRoutes);
 
 app.listen(4000, () => console.log("VISE API running on port 4000"));
+
+// Initialize using the connection string injected by Azure
+ appInsights
+ .setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
+ .setAutoDependencyCorrelation(true)
+ .setAutoCollectRequests(true)
+ .setAutoCollectPerformance(true)
+ .setAutoCollectExceptions(true)
+ .setAutoCollectDependencies(true)
+ .setAutoCollectConsole(true, true)
+ .setUseDiskRetryCaching(true)
+ .start();
+ const client = appInsights.defaultClient;
+ client.context.tags[client.context.keys.cloudRole] = "my-node-api"; //
+
+  client.trackEvent({ name: "server_started", properties: { environment:
+ "production" } });
