2fc6e8394c133786592f58ed0744c440293f97e1
wilches <willchezandressalas@gmail.com>
2025-11-16 11:05:05 -0500

    Readme actualizado con los pasos para la ejecución del proyecto vise

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require("./instrumentation.js");
+
 // server.js
 const appInsights = require("applicationinsights");
 const express = require("express");
 const bodyParser = require("body-parser");
+
 
 const app = express();
 app.use(bodyParser.json());
